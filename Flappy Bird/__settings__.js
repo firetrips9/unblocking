7c6203c3906002a318dface91e558b1b94d8780a
firetrips9 <nathaniel.lindo@outlook.com>
2024-10-20 21:42:29 +0100

    that bird be flappin

---
+++ Flappy Bird/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2094796.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 200427634, 200427673, 200427633, 200427674, 200427635, 200427639, 200427642, 200427668, 200427646, 200427647, 200427649, 200427648, 200427676, 200427675, 200427672 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
